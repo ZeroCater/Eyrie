20955926c80e31e4fdff7085e8658c09869e39f9
Hamed Ahmadi <hamed@zerocater.com>
2017-04-19 16:07:24 -0700

    fix redirect

--- interface/urls.py
+++ interface/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^add/(?P<full_name>[\w/.-]+)$', views.ProcessRepo, name='process_repo'),
     url(r'^repos$', views.RepoListView.as_view(), name='repo_list'),
     url(r'^repo/(?P<full_name>[\w/.-]+)/delete$', views.RepoDeleteView.as_view(), name='repo_delete'),
-    url(r'^repo/(?P<full_name>[\w\-_]+/[\w\-_]+)/(?P<path>[\w/\-._]+)?$',
+    url(r'^repo/(?P<full_name>[\w\-_]+/[\w\-_]+)(?P<path>/?[\w/\-._]+)?$',
         views.RepoDetailView.as_view(), name='repo_detail'),
     url(r'^logout$', views.LogoutView, name='logout'),
     url(r'^about$', TemplateView.as_view(template_name='about.html')),
