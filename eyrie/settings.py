baafb2d98267d071d94ef40e031c805c449e8a99
Brandon Birkholz <brandonjbirkholz@gmail.com>
2017-05-22 12:17:51 -0700

    Add apex domain to ALLOWED_HOSTS

--- eyrie/settings.py
+++ eyrie/settings.py
@@ -158,7 +158,7 @@ if not DEBUG:  # pragma: no cover
         'raven.contrib.django.raven_compat'
     ]
 
-    ALLOWED_HOSTS = ['www.eyrieapp.com']
+    ALLOWED_HOSTS = ['www.eyrieapp.com', 'eyrieapp.com']
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     SECURE_CONTENT_TYPE_NOSNIFF = True
